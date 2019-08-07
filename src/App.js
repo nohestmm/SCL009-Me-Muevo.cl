0ea7abf606ecf2937897883fd8b5ee180f3bd5df
nohestmm <nohestmm@gmail.com>
2019-08-06 23:26:59 -0400

    pushing deploy with master change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 /*Templates && Components*/
 import TemplateInfo from './views/TemplateInfo';
 import TemplateLaw from './views/TemplateLaw';
@@ -8,7 +8,7 @@ import TemplateRegister from './views/TemplateRegister';
 import TemplateWhoWeAre from './views/TemplateWhoWeAre';
 import TemplateHome from './views/TemplateHome';
 import './App.css';
-import Navbartemplate from './components/Navbartemplate';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -19,25 +19,6 @@ class App extends Component {
 
     return (
       <>
-<<<<<<< HEAD
-
-        <BrowserRouter>
-                  <div >
-                  <Navbartemplate/>
-            {/* <div  >
-
-              <div className="input-search"><input
-                type="search"
-                className="form-control"
-                placeholder="🔎 Buscar" />
-              </div>
-              <ul className="ĺist-group">
-              <li className="list-group-item"><Link to="/">Inicio</Link></li>
-                <li className="list-group-item"><Link to="/ley-ricarte-soto">Ley Ricarte Soto</Link></li>
-                <li className="list-group-item"><Link to="/noticias">Noticias</Link></li>
-                <li className="list-group-item"><Link to="/registro">Registro de Pacientes</Link></li>
-                <li className="list-group-item"><Link to="/quienes-somos">Quiénes Somos</Link></li>
-=======
         <BrowserRouter>
 
           <div className="App row">
@@ -45,7 +26,7 @@ class App extends Component {
               <nav className="navbar navbar-light bg-light">
                 {/* <a className="navbar-brand" href> */}
                   <div className="logo">
-                    <Link to="/"><img src="http://www.memuevo.cl/wp-content/themes/newmemuevo/img/logo.png" width="90" height="70" className="d-inline-block align-top" alt="logo de la fundación" /></Link>
+                    <Link to="/"><img src="https://www.memuevo.cl/wp-content/themes/newmemuevo/img/logo.png" width="90" height="70" className="d-inline-block align-top" alt="logo de la fundación" /></Link>
                   </div>
                 {/* </a> */}
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -64,18 +45,12 @@ class App extends Component {
                     <Link to="/registro"><h3 className="list-group-item list-group-item-action">GES/Auge</h3></Link>
                     <Link to="/registro"><h3 className="list-group-item list-group-item-action">Regístrate como paciente</h3></Link>
                     <Link to="/noticias"><h3 className="list-group-item list-group-item-action">Noticias y Eventos</h3></Link>
->>>>>>> d30db577a05be0ec4da1ae3b07defef7ab63e7a5
 
                     <div className="rrss">
                       <a href="https://es-la.facebook.com/memuevo.cl/"><img src="https://cdn2.iconfinder.com/data/icons/social-icon-3/512/social_style_3_facebook-512.png" className="icon" alt="logo facebook" /></a>
                       <a href="https://twitter.com/memuevocl?lang=es"><img src="https://cdn2.iconfinder.com/data/icons/social-icon-3/512/social_style_3_twiter-512.png" className="icon" alt="logo twitter"></img></a>
                       <a href="https://www.youtube.com/channel/UCyl9QdUUFMCDh0HARVzvvwg"><img src="https://cdn2.iconfinder.com/data/icons/social-icon-3/512/social_style_3_youtube-512.png" className="icon" alt="logo youtube"></img></a>
 
-<<<<<<< HEAD
-              </ul>
-            </div> */}
-            <div>
-=======
                     </div>
                     <div className="rrss">
                       <a className="" href="tel:+56987142470"><h3>Tel: +56 9 87142470</h3></a>
@@ -86,7 +61,6 @@ class App extends Component {
               
             </div>
             <div className="content col-sm-12 col-md-9">
->>>>>>> d30db577a05be0ec4da1ae3b07defef7ab63e7a5
               <Switch>
                 <Route exact path='/' component={TemplateHome} />
                 <Route exact path='/artritis-reumatoide' component={TemplateInfo} />
--- src/views/TemplateHome.js
+++ src/views/TemplateHome.js
@@ -5,26 +5,10 @@ import '../views/components.css';
 const TemplateHome = () => {
     return (
         <div>
-<<<<<<< HEAD
-        
-            <h1>Bienvenido a Me muevo</h1>
-            <section className="infoTemplateHome">
-                <div className = "col-sm-12 col-md-8">
-                    <iframe width="560" height="315" 
-                    src="https://www.youtube.com/embed/t485RIj4-og" 
-                    frameBorder="0" 
-                    allow="accelerometer; 
-                    autoplay; 
-                    encrypted-media; 
-                    gyroscope; 
-                    picture-in-picture" 
-                    allowFullScreen title="video"/>
-=======
             <h1>Bienvenido a Memuevo</h1>
             {/* <section className="infoTemplateHome">
                 <div className = "col-sm-12 col-md-8">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/t485RIj4-og" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="video"></iframe>
->>>>>>> d30db577a05be0ec4da1ae3b07defef7ab63e7a5
                 </div>
                 <div className= "col-sm-12 col-md-4">
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam laboriosam minus architecto nulla id consequatur eos porro culpa odio possimus? Iusto doloremque doloribus hic accusamus cumque libero repellat sit maiores? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam et atque adipisci, aliquid molestias ipsum libero debitis quos harum alias dolorum culpa optio consequatur veniam commodi vero? Odio, fugiat deleniti!
