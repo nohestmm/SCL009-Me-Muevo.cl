fec8a37003e1dd31b590836eae6a6d98e35fbe39
betanyeli <betangelii@gmail.com>
2019-08-07 16:04:33 -0400

    deleting cards bootstrap

---
+++ src/views/TemplateGes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TemplateGes = () => {
+    
+        return (
+<div>
+    <h1>GES/Auge</h1>
+</div>
+        )
+    
+}
+
+export default TemplateGes;
--- src/views/TemplateNews.js
+++ src/views/TemplateNews.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import data from './data.json'
+import './components.css'
 
 class TemplateNews extends Component{
 constructor(){
@@ -13,9 +14,19 @@ render(){
         const news = this.state.dataNews.map((element, index)=>{
       
                 return (  
+                        // <div>
+                        //         <ul>
+                        //                 <li>
+                        //                         <h5><a href={element.url}>{element.title}</a></h5>
+                        //                         <img src={element.img} className="img-fluid" alt="me muevo"></img>
+                        //                         <h6>{element.date} - {element.type}</h6>
+                        //                         <p>{element.description}</p>
+                        //                 </li>
+                        //         </ul>
+                        // </div>
                
-                <div className="card col-sm-12 col-md-4 col-lg-2 mt-3">
-                  <img src={element.img} className="card-img-top" alt="me muevo"></img>
+                <div className="card mt-3">
+                  {/* <img src={element.img} className="card-img-top" alt="me muevo"></img> */}
                   <div className="card-body">
                   <h5 className="card-title"><a className="card-link" href={element.url}>{element.title}</a></h5>
                   <h6 className="card-subtitle mb-2 text-muted">{element.date} - {element.type}</h6>
